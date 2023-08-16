5d005559a61a61128c59223d750080a00b10b40a
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2023-08-16 15:44:04 +0000

    domainGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    // console.log(pronoun[i]);
+    for (let j = 0; j < adj.length; j++) {
+      // console.log(adj[j]);
+      for (let k = 0; k < noun.length; k++) {
+        // console.log(noun[k]);
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
 };
